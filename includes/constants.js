0afc4678b1b0ce31f39d50625d4425306d96b2db
Anandhan <SA06197@de.corp.devoteam.com>
2024-02-13 10:45:30 +0100

    First commit of all dataform files

---
+++ includes/constants.js
@@ -0,0 +1,13 @@
+const pj_orch = dataform.projectConfig.vars.base_pj_orch + '-' + dataform.projectConfig.vars.executionSetting;
+const pj_lz   = dataform.projectConfig.vars.base_pj_lz  + '-' + dataform.projectConfig.vars.executionSetting;
+const pj_raw  = dataform.projectConfig.vars.base_pj_raw + '-' + dataform.projectConfig.vars.executionSetting;
+const pj_dwh  = dataform.projectConfig.vars.base_pj_dwh + '-' + dataform.projectConfig.vars.executionSetting;
+const pj_dm   = dataform.projectConfig.vars.base_pj_dm  + '-' + dataform.projectConfig.vars.executionSetting;
+
+module.exports = {
+    pj_orch,
+    pj_lz,
+    pj_raw,
+    pj_dwh,
+    pj_dm
+};
